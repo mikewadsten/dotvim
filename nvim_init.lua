1134d1ed2a2bbc242507e99a18c75e89d7bdf7fb
Mike Wadsten <mikewadsten@gmail.com>
2022-05-13 09:45:14 -0500

    nvim_init.lua: If lspconfig is missing, PlugInstall and try again

--- nvim_init.lua
+++ nvim_init.lua
@@ -1,3 +1,16 @@
 if vim.fn.executable('pylsp') then
-    require('lspconfig').pylsp.setup{}
+    local function setup_lsp_servers(lsp_)
+        lsp_.pylsp.setup{}
+    end
+
+    local lsp_plugin_available, lsp = pcall(require, 'lspconfig')
+    if lsp_plugin_available then
+        setup_lsp_servers(lsp)
+    else
+        print("lspconfig not found? Let's run PlugInstall...")
+        vim.fn.timer_start(50, function()
+            vim.cmd([[PlugInstall]])
+            setup_lsp_servers(require('lspconfig'))
+        end)
+    end
 end
