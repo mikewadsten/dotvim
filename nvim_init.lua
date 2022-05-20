c5994fb2c648c109c7c8dd61efcc66ce552ab244
Mike Wadsten <mikewadsten@gmail.com>
2022-05-20 17:41:34 -0500

    nvim_init.lua: mappings, generalize the code a bit

--- nvim_init.lua
+++ nvim_init.lua
@@ -1,16 +1,28 @@
-if vim.fn.executable('pylsp') then
-    local function setup_lsp_servers(lsp_)
-        lsp_.pylsp.setup{}
-    end
+local on_attach = function(client, bufnr)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+    -- Set SuperTab to prefer omnifunc over autocompletion
+    vim.b[bufnr].SuperTabContextTextOmniPrecedence = {'&omnifunc', '&completefunc'}
+    vim.b[bufnr].SuperTabDefaultCompletionType = "context"
 
-    local lsp_plugin_available, lsp = pcall(require, 'lspconfig')
-    if lsp_plugin_available then
-        setup_lsp_servers(lsp)
-    else
-        print("lspconfig not found? Let's run PlugInstall...")
-        vim.fn.timer_start(50, function()
-            vim.cmd([[PlugInstall]])
-            setup_lsp_servers(require('lspconfig'))
-        end)
+    -- TODO mappings
+end
+
+local function setup_lsp(lsp_)
+    if vim.fn.executable('pylsp') then
+        lsp_.pylsp.setup{
+            on_attach = on_attach,
+        }
     end
+end
+
+local lsp_plugin_available, lsp = pcall(require, 'lspconfig')
+if lsp_plugin_available then
+    setup_lsp(lsp)
+else
+    print("lspconfig not found? Let's run PlugInstall...")
+    vim.fn.timer_start(50, function()
+        vim.cmd([[PlugInstall]])
+        setup_lsp(require('lspconfig'))
+    end)
 end
